763fbdc1555bc239adc15f2c0daf89894eca3efd
liuhangkai <liuhangkai@liuhangkaidemacbook-air.local>
2016-10-15 20:52:40 +0800

    Rggggg

---
+++ Quarz2D/testPerson.h
@@ -0,0 +1,13 @@
+//
+//  testPerson.h
+//  Quarz2D
+//
+//  Created by liuhangkai on 16/10/15.
+//  Copyright © 2016年 liuhangkai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testPerson : NSObject
+
+@end
---
+++ Quarz2D/testPerson.m
@@ -0,0 +1,15 @@
+//
+//  testPerson.m
+//  Quarz2D
+//
+//  Created by liuhangkai on 16/10/15.
+//  Copyright © 2016年 liuhangkai. All rights reserved.
+//
+
+#import "testPerson.h"
+
+@implementation testPerson
+-(void)test{
+    
+}
+@end
